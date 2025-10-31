d5c292241e698d3f28202b3476b3630f40fc1d89
Ati <bricegomis@users.noreply.github.com>
2025-10-31 19:24:32 +0100

    test

--- backend/api/LaCarotte.API/Controllers/CarotteController.cs
+++ backend/api/LaCarotte.API/Controllers/CarotteController.cs
@@ -1,84 +1,46 @@
-using System.Diagnostics;
+using System.ComponentModel.DataAnnotations;
 using LaCarotte.API.Manager;
+using LaCarotte.API.Manager.Interfaces;
 using LaCarotte.API.Models;
+using LaCarotte.API.Models.Dto;
 using Microsoft.AspNetCore.Mvc;
-using Swashbuckle.AspNetCore.Annotations;
 
-namespace LaCarotte.API.Controllers
+namespace LaCarotte.API.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class CarotteController(
+    ICarotteService carotteService)
+    : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class CarotteController : ControllerBase
+    [HttpGet]
+    public async Task<IEnumerable<CarotteDto>> ListAsync()
     {
-        private readonly ILogger<CarotteController> _logger;
-        private readonly ICarotteManager _carotteManager;
+        return await carotteService.List();
+    }
 
-        public CarotteController(ILogger<CarotteController> logger,
-                                ICarotteManager carotteManager)
-        {
-            _logger = logger;
-            _carotteManager = carotteManager;
-        }
+    [HttpGet("{id}")]
+    [Produces(typeof(CarotteDto))]
+    public async Task<CarotteDto> Get([FromRoute, Required]string id)
+    {
+        return await carotteService.Get(id);
+    }
 
-        [HttpGet(Name = "[controller]-GetAll")]
-        [SwaggerOperation(OperationId = "[controller]-GetAll")]
-        public async Task<IEnumerable<Carotte>> GetcarotteAsync()
-        {
-            var stopwatch = new Stopwatch();
-            stopwatch.Start();
+    [HttpPost]
+    public async Task CreateCarotte([FromBody, Required]CarotteCreateDto item)
+    {
+        await carotteService.Create(item);
+    }
 
-            _logger.LogDebug("GetcarotteAsync start");
-            var carottes = await _carotteManager.GetAllCarottes();
+    [HttpPut]
+    public async Task UpdateCarotte([FromBody, Required]CarotteUpdateDto item)
+    {
+        await carotteService.Update(item);
+    }
 
-            stopwatch.Stop();
-            _logger.LogDebug("GetcarotteAsync: {stopwatch.ElapsedMilliseconds} ms", stopwatch.ElapsedMilliseconds);
-
-            return carottes;
-        }
-
-        [HttpGet("{id}", Name = "GetById")]
-        [Produces(typeof(Carotte))]
-        [SwaggerResponse((int)System.Net.HttpStatusCode.OK, Type = typeof(Carotte))]
-        public async Task<Carotte> GetCarotte(string id)
-        {
-            _logger.LogDebug("GetCarotte called with id: {id}", id);
-            return await _carotteManager.GetCarotte(id);
-        }
-
-        [HttpPost(Name = "Create")]
-        [SwaggerResponse((int)System.Net.HttpStatusCode.OK)]
-        public async Task CreateCarotte(Carotte item)
-        {
-            _logger.LogDebug("CreateCarotte called with id: {id}", item?.Id ?? "null");
-            if (item == null) return;
-            await _carotteManager.CreateCarotte(item);
-        }
-
-        [HttpPut(Name = "Update")]
-        [SwaggerResponse((int)System.Net.HttpStatusCode.OK)]
-        public async Task UpdateCarotte(Carotte item)
-        {
-            _logger.LogDebug("UpdateCarotte called with id: {id}", item?.Id ?? "null");
-            if (item == null) return;
-            await _carotteManager.UpdateCarotte(item);
-        }
-
-        [HttpPut("finish", Name = "Finish")]
-        [SwaggerResponse((int)System.Net.HttpStatusCode.OK)]
-        public async Task FinishCarotte(Carotte item)
-        {
-            _logger.LogDebug("FinishCarotte called with id: {id}", item?.Id ?? "null");
-            if (item == null) return;
-            await _carotteManager.FinishCarotte(item);
-        }
-
-        [HttpDelete("{id}", Name = "Delete")]
-        [SwaggerResponse((int)System.Net.HttpStatusCode.OK)]
-        public async Task DeleteCarotte(string id)
-        {
-            _logger.LogDebug("DeleteCarotte called with id: {id}", id ?? "null");
-            if (id == null) return;
-            await _carotteManager.DeleteCarotte(id);
-        }
+    [HttpDelete("{id}")]
+    public async Task DeleteCarotte([FromRoute, Required]string id)
+    {
+        await carotteService.Delete(id);
     }
 }
--- backend/api/LaCarotte.API/Program.cs
+++ backend/api/LaCarotte.API/Program.cs
@@ -1,36 +1,162 @@
-using Serilog;
+using System.Security.Cryptography.X509Certificates;
+using Ati.API.Common.Constants;
+using Ati.API.Common.Controllers;
+using Ati.API.Common.Extensions;
+using Ati.API.Common.Models.Documents.Interfaces;
+using Ati.API.Common.Services.Interfaces;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.OpenApi;
+using Microsoft.OpenApi.Models;
+using Raven.Client.Documents;
+using Raven.Client.Documents.Conventions;
+using Raven.Client.Documents.Indexes;
+using Scalar.AspNetCore;
 
-namespace LaCarotte.API
+var builder = WebApplication.CreateBuilder(args);
+
+// Configure OpenAPI
+builder.Services.AddOpenApi(options =>
 {
-    internal class Program
-    {
-        private static void Main(string[] args)
+    options.AddDocumentTransformer(
+        (document, _, _) =>
         {
-            Log.Logger = new LoggerConfiguration()
-                .WriteTo.Console()
-                .CreateLogger();
-
-            try
+            document.Info = new()
             {
-                Log.Information("Starting up");
-                CreateHostBuilder(args).Build().Run();
-            }
-            catch (Exception ex)
-            {
-                Log.Fatal(ex, "Application start-up failed");
-            }
-            finally
-            {
-                Log.CloseAndFlush();
-            }
+                Title = "La Carotte API",
+                Version = "v1"
+            };
+            return Task.CompletedTask;
         }
+    );
+    options.AddDocumentTransformer<BearerSecuritySchemeTransformer>();
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .UseSerilog()
-                .ConfigureWebHostDefaults(webBuilder =>
+builder
+    .Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        options.Authority = builder.Configuration[EnvConstants.AuthSettingsJwtIssuer];
+        options.Audience = builder.Configuration[EnvConstants.AuthSettingsJwtAudience];
+        options.RequireHttpsMetadata = false;
+        options.TokenValidationParameters =
+            new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidIssuer = builder.Configuration[EnvConstants.AuthSettingsJwtIssuer],
+            };
+    });
+
+builder.Services.AddControllers();
+
+#region RavenDB
+var services = builder.Services;
+var ravenDbServer = builder.Configuration["RavenDBServer"];
+var ravenDbName = builder.Configuration["RavenDBDbName"];
+var ravenDbCertBase64 = builder.Configuration["RavenDBCertificateBase64"];
+var ravenDbCertPassword = builder.Configuration["RavenDBCertificatePassword"];
+if (ravenDbCertBase64 == null || ravenDbCertPassword == null || ravenDbServer == null || ravenDbName == null)
+{
+    throw new InvalidOperationException("RavenDB configuration is missing");
+}
+var certBytes = Convert.FromBase64String(ravenDbCertBase64);
+var ravenDbCertificate = new X509Certificate2(certBytes, ravenDbCertPassword);
+services.AddSingleton<IDocumentStore>(sp =>
+{
+    var store = new DocumentStore
+    {
+        Urls = [ravenDbServer],
+        Database = ravenDbName,
+        Conventions =
+        {
+            MaxNumberOfRequestsPerSession = 10
+        },
+        Certificate = ravenDbCertificate,
+    };
+
+    store.Conventions.FindCollectionName = type =>
+    {
+        if (type.ToString().EndsWith("Document"))
+        {
+            return type.Name[..^"Document".Length] + "s";
+        }
+        return DocumentConventions.DefaultGetCollectionName(type);
+    };
+
+    var dateTimeService = sp.GetService<IDateTimeService>();
+    store.OnBeforeStore += (sender, args) =>
+    {
+        if (args.Entity is IDateTracked dateTracked)
+        {
+            // If DateCreated is the default value, it's a new document
+            if (dateTracked.DateCreated == DateTimeOffset.MinValue)
+            {
+                dateTracked.DateCreated = dateTimeService?.GetNow() ?? DateTimeOffset.UtcNow;
+            }
+            dateTracked.DateModified = dateTimeService?.GetNow() ?? DateTimeOffset.UtcNow;
+        }
+    };
+
+    store.Initialize();
+
+    return store;
+});
+services.AddScoped(sp =>
+{
+    var store = sp.GetRequiredService<IDocumentStore>();
+    return store.OpenAsyncSession();
+});
+#endregion
+
+builder.Services.AddAttributedServices(typeof(AuthController).Assembly);
+builder.Services.AddAttributedServices(typeof(Program).Assembly);
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi().CacheOutput();
+    app.MapScalarApiReference();
+    app.MapGet("/", () => Results.Redirect("/scalar/v1")).ExcludeFromDescription();
+}
+
+app.UseAuthentication();
+
+app.UseRouting();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
+
+internal sealed class BearerSecuritySchemeTransformer(
+    IAuthenticationSchemeProvider authenticationSchemeProvider
+) : IOpenApiDocumentTransformer
+{
+    public async Task TransformAsync(
+        OpenApiDocument document,
+        OpenApiDocumentTransformerContext context,
+        CancellationToken cancellationToken
+    )
+    {
+        var authenticationSchemes = await authenticationSchemeProvider.GetAllSchemesAsync();
+        if (authenticationSchemes.Any(authScheme => authScheme.Name == "Bearer"))
+        {
+            var requirements = new Dictionary<string, OpenApiSecurityScheme>
+            {
+                ["Bearer"] = new OpenApiSecurityScheme
                 {
-                    webBuilder.UseStartup<Startup>();
-                });
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer", // "bearer" refers to the header name here
+                    In = ParameterLocation.Header,
+                    BearerFormat = "Json Web Token",
+                },
+            };
+            document.Components ??= new OpenApiComponents();
+            document.Components.SecuritySchemes = requirements;
+        }
     }
 }
